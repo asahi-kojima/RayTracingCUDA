7f9121f4eadcd9cda832fabf311b98031ded92bc
asahi kojima <ubutara12@gmail.com>
2025-11-20 23:08:25 +0900

    meshが返すAABBのバグを修正

--- RayTracingCUDA/mesh.cpp
+++ RayTracingCUDA/mesh.cpp
@@ -55,18 +55,5 @@ Mesh::Mesh(const Mesh& mesh)
 
 AABB Mesh::getAABB() const
 {
-	Vec3 minPosition = mVertexArray[0].position;
-	Vec3 maxPosition = mVertexArray[0].position;
-	for (const Vertex& vertex : mVertexArray)
-	{
-
-		minPosition[0] = fminf(minPosition[0], vertex.position[0]);
-		minPosition[1] = fminf(minPosition[1], vertex.position[1]);
-		minPosition[2] = fminf(minPosition[2], vertex.position[2]);
-		maxPosition[0] = fmaxf(maxPosition[0], vertex.position[0]);
-		maxPosition[1] = fmaxf(maxPosition[1], vertex.position[1]);
-		maxPosition[2] = fmaxf(maxPosition[2], vertex.position[2]);
-	}
-
-	return AABB(minPosition, maxPosition);
+	return mAABB;
 }
